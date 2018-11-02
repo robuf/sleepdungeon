1eab6328ab8eba662ad1daa71442981051c581db
Robert Ufer <robert.ufer@mailbox.tu-dresden.de>
2018-11-02 20:11:18 +0100

    Spielkonstanten hinzugefügt

---
+++ src/base/game_constants.py
@@ -0,0 +1,36 @@
+
+class SpriteType(object):
+    PLAYER = 0
+    ENEMY = 1
+    ITEM = 2
+    STATIC = 3
+    ENTITY = 4
+    GHOST = 5
+
+
+class ZIndex(object):
+    BACKROUND = -128
+    GROUND = -64
+    PLAYGROUND = 0
+    SKY = 64
+
+    @staticmethod
+    def is_background(index: int) -> bool:
+        return index <= ZIndex.BACKROUND
+
+    @staticmethod
+    def is_ground(index: int) -> bool:
+        return ZIndex.BACKROUND < index <= ZIndex.GROUND
+
+    @staticmethod
+    def is_playground(index: int) -> bool:
+        return ZIndex.GROUND < index <= ZIndex.PLAYGROUND
+
+
+    @staticmethod
+    def is_sky(index: int) -> bool:
+        return ZIndex.PLAYGROUND < index <= ZIndex.SKY
+
+    @staticmethod
+    def is_ui(index: int) -> bool:
+        return ZIndex.SKY < index
