a7801d08f696eb38754b4bb0be396992e6d2519c
Karsten Lehmann <mail@kalehmann.de>
2018-11-02 21:50:26 +0100

    Added code for initializing pygame

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,17 @@
+import pygame
+import sys
+
+def initialize():
+    if not pygame.font:
+        print('Error, pygame.font not found!')
+        sys.exit(1)
+    if not pygame.mixer:
+        print('Error, pygame.mixer not found!')
+        sys.exit(1)
+        # Pre initialize the mixer with a smaller buffer size, this solves
+        # problems.
+    pygame.mixer.pre_init(22050, -16, 2, 512)
+    pygame.init()
 
 def main():
-    pass
+    initialize()
