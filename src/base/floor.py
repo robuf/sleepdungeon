bfa84a716340e3fcefbe3cb7c5841ae1954fbbab
Karsten Lehmann <mail@kalehmann.de>
2018-11-03 11:16:17 +0100

    Started implementing floor configuration file

--- src/base/floor.py
+++ src/base/floor.py
@@ -1,5 +1,9 @@
-
+import os
+import sys
 
 class Floor(object):
-    def __init__(self):
+    def __init__(self, floor_file: str):
+        if not os.path.isfile(floor_file):
+            print("Could not load floor file")
+            sys.exit(1)
         self.rooms = []
