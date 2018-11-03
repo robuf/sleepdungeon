a65c285699e5f740390f64ec72868a360d359d3a
Lars Westermann <lars-westermann@live.de>
2018-11-03 10:33:40 +0100

    Add simple level file

--- src/base/room.py
+++ src/base/room.py
@@ -1,4 +1,7 @@
 from ..sprites.player import Player
+from ..res import IMG_DIR, LEVEL_DIR
+from typing import List, Optional
+import pygame
 
 
 class Room(object):
@@ -6,16 +9,30 @@ class Room(object):
         self.sprites = list()
         self.room = list()
         with open(path, 'r') as f:
-             for line in f.readlines():
-                line = line.split()
-                pos_x = int(line[0])
-                pos_y = int(line[1])
-                class_name = str(line[2])
-                self.room.append([pos_x, pos_y, class_name])
+            for line in f.readlines():
+                line = line.split(" ")
+                x = Room.parse(line)
+                if x is not None:
+                    self.sprites.append(x)
 
-    def add_sprite_to_list(self, object):
-        self.sprites.append(object)
+    @staticmethod
+    def parse(token: List[str]) -> Optional[object]:
+        if token[0] == "BACKGROUND":
+            name = token[1]
+            surface = pygame.image.load(IMG_DIR + "room/room_" + name + ".png")
 
+        elif token[0] == "DOOR":
+            side = token[1]
+            next_room = token[2]
+            surface = pygame.image.load(IMG_DIR + "room/doors.png")
+
+        elif token[0] == "PLAYER":
+            x = int(token[1])
+            y = int(token[2])
+
+        return None
+
+    """
     def create_type(self):
         for line in self.room:
             if line[2] == TYPE_PLAYER:
@@ -30,6 +47,4 @@ class Room(object):
                 self.sprites.append(entity)
             if line[2] == TYPE_GHOST:
                 self.sprites.append(ghost)
-
-
-
+    """
