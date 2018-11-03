e26e73a4f30f07acf208efb155cb01757145b989
Lars Westermann <lars-westermann@live.de>
2018-11-03 11:51:18 +0100

    Add path finding test function

--- src/util/path_finder.py
+++ src/util/path_finder.py
@@ -1,18 +1,17 @@
-from typing import Tuple, List, Set, Optional
+from typing import Tuple, List, Set, Optional, Dict
 from enum import Enum
 
-from ..base.game_constants import Facing
-
 Point = Tuple[int, int]
-Entity = Tuple[int, int,]
+Entity = Tuple[int, int, int]
 
 
 def find_path(source: Entity, target: Point, obstacles: Set[Point], distance: int = 0) -> Optional[List['Action']]:
+    found = {}
     return None
 
 
 class Action:
-    def __init__(self, action_type: 'ActionType', direction: Optional[Facing] = None):
+    def __init__(self, action_type: 'ActionType', direction: Optional[int] = None):
         self.type = action_type
         self.direction = direction
 
@@ -20,3 +19,16 @@ class Action:
 class ActionType(Enum):
     MOVE = 0
     TURN = 1
+
+
+if __name__ == "__main__":
+    source = 1, 1, 0
+    target = 4, 3
+
+    path = find_path(source, target, set(), 0)
+
+    if path is None:
+        print("No path found")
+    else:
+        for action in path:
+            print(str(action.type) + str(action.direction))
