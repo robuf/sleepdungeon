3e915632d8d85708841210db04d9abdbc712a09b
MartinOehme <martin-oehme@gmx.de>
2018-11-03 11:09:04 +0100

    Attacks

--- src/base/sprites.py
+++ src/base/sprites.py
@@ -6,8 +6,11 @@ class Sprites(list):
     def __init__(self):
         super().__init__()
 
-    def get_sprites_in_room(self) -> Sprite:
-        pass
+    def get_sprites_in_room(self) -> int:
+        return len(self)
 
     def check_for_sprite(self) -> Sprite:
+        pass
+
+    def get_sprite_in_front(self):
         pass
