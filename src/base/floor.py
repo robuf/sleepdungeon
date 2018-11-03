597eac50917dc53655d6dc5d16ce23c9289f8d5b
Karsten Lehmann <mail@kalehmann.de>
2018-11-03 11:27:03 +0100

    Removed floor file

--- src/base/floor.py
+++ src/base/floor.py
@@ -2,8 +2,5 @@ import os
 import sys
 
 class Floor(object):
-    def __init__(self, floor_file: str):
-        if not os.path.isfile(floor_file):
-            print("Could not load floor file")
-            sys.exit(1)
+    def __init__(self):
         self.rooms = []
