47e5e3a411df90a172edc71a12ce7b760b3076d3
Lars Westermann <lars-westermann@live.de>
2018-11-04 16:23:11 +0100

    Add light stone texture

--- src/sprites/stone.py
+++ src/sprites/stone.py
@@ -74,3 +74,37 @@ class BreakableStone(Sprite):
                 cls.tile_size
             )
         )
+
+
+class MovableStone(Sprite):
+    __BASE_SURFACE: pygame.Surface = None
+    __SURFACE: pygame.Surface = None
+
+    def __init__(self, x: int, y: int):
+        super().__init__()
+        self.width = 1
+        self.height = 1
+        self.position = Position(x, y)
+
+    def update(self, context: Context):
+        pass
+
+    @property
+    def image(self) -> pygame.Surface:
+        return Stone.__SURFACE
+
+    @property
+    def sprite_type(self) -> SpriteType:
+        return SpriteType.STATIC
+
+    @classmethod
+    def update_render_context(cls, render_context: RenderContext):
+        if not cls.__BASE_SURFACE:
+            cls.__BASE_SURFACE = pygame.image.load(IMG_DIR + "room/stone/stone_moveable.png")
+        cls.__SURFACE = pygame.transform.smoothscale(
+            cls.__BASE_SURFACE,
+            (
+                cls.tile_size,
+                cls.tile_size
+            )
+        )
