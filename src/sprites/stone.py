cc78fb640ab8d5ae95b7e4ac70fd24c18a77cb87
Lars Westermann <lars-westermann@live.de>
2018-11-04 15:23:55 +0100

    Add breakable stone

--- src/sprites/stone.py
+++ src/sprites/stone.py
@@ -40,3 +40,37 @@ class Stone(Sprite):
                 cls.tile_size
             )
         )
+
+
+class BreakableStone(Sprite):
+    __BASE_SURFACE: pygame.Surface = None
+    __SURFACE: pygame.Surface = None
+
+    def __init__(self, x: int, y: int):
+        super().__init__()
+        self.width = 1
+        self.height = 1
+        self.position = Position(x, y)
+
+    def update(self, context: Context):
+        pass
+
+    @property
+    def image(self) -> pygame.Surface:
+        return BreakableStone.__SURFACE
+
+    @property
+    def sprite_type(self) -> SpriteType:
+        return SpriteType.STATIC
+
+    @classmethod
+    def update_render_context(cls, render_context: RenderContext):
+        if not cls.__BASE_SURFACE:
+            cls.__BASE_SURFACE = pygame.image.load(IMG_DIR + "room/stone/stone_breakable.png")
+        cls.__SURFACE = pygame.transform.smoothscale(
+            cls.__BASE_SURFACE,
+            (
+                cls.tile_size,
+                cls.tile_size
+            )
+        )
