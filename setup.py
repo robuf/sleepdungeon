6454bafe58cecaceaf3f189ac9140730d7d155e2
Karsten Lehmann <mail@kalehmann.de>
2018-11-04 18:22:02 +0100

    Bundled everything to a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+	entry_points={
+		'console_scripts' : [
+			'sleepdungeon=sleepdungeon:main'
+		]
+	}
+)
