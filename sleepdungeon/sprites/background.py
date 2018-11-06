b28941854ca008ef2c5c8ecab07f42b0bf2caae3
Karsten Lehmann <mail@kalehmann.de>
2018-11-06 23:52:04 +0100

    Fixed background
    You can now load new background images at every point of the
    initialization of the game.

--- sleepdungeon/sprites/background.py
+++ sleepdungeon/sprites/background.py
@@ -12,11 +12,20 @@ from ..res import IMG_DIR
 class Background(Sprite):
     __BASE_SURFACES = {}
     __SURFACES = {}
+    __RENDER_CONTEXT = None
 
     def __init__(self, name: str):
         super().__init__()
         if name not in Background.__BASE_SURFACES:
             Background.__BASE_SURFACES[name] = pygame.image.load(IMG_DIR + "room/room_" + name + ".png")
+        if Background.__RENDER_CONTEXT and name not in Background.__SURFACES:
+            Background.__SURFACES[name] = pygame.transform.smoothscale(
+                Background.__BASE_SURFACES[name],
+                (
+                    Background.__RENDER_CONTEXT.resolution[0] - Background.sidebar_width,
+                    Background.__RENDER_CONTEXT.resolution[1]
+                )
+            )
         self.name = name
         self.width = 13
         self.height = 9
@@ -27,6 +36,7 @@ class Background(Sprite):
 
     @classmethod
     def update_render_context(cls, render_context):
+        cls.__RENDER_CONTEXT = render_context
         for name, base in cls.__BASE_SURFACES.items():
             cls.__SURFACES[name]= pygame.transform.smoothscale(
                 cls.__BASE_SURFACES[name],
