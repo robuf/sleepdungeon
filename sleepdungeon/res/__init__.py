b6042e89ae5225ff19dc48806af7ada62f67f6f8
Karsten Lehmann <ka.lehmann@yahoo.com>
2018-11-11 13:29:50 +0100

    Made resource loading compatible to PyInstaller

--- sleepdungeon/res/__init__.py
+++ sleepdungeon/res/__init__.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 from os.path import dirname, abspath
 import inspect
 
-RESOURCE_DIR = dirname(
-    abspath(inspect.getfile(inspect.currentframe()))
-)
+try:
+    RESOURCE_DIR = sys._MEIPASS + "/sleepdungeon/res"
+except:
+    RESOURCE_DIR = dirname(
+        abspath(inspect.getfile(inspect.currentframe()))
+    )
 
 FONT_DIR = RESOURCE_DIR + "/font/"
 IMG_DIR = RESOURCE_DIR + "/img/"
